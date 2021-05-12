4f48d9f041410bd2c50e0cec97ade58ad6e69602
Nikica Dadic <75340386+Nick2099@users.noreply.github.com>
2021-05-13 00:07:36 +0200

    updated

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const fName = "FirstName";
+const lName = "LastName";
+const num = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}
+    </h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
