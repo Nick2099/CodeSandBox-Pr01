cc876ec79cfd124b28ce6a01646277cdca68b493
Nikica Dadic <75340386+Nick2099@users.noreply.github.com>
2021-05-16 21:34:50 +0200

    few changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 const fName = "FirstName";
 const lName = "LastName";
 const num = 7;
+var currentDate = new Date();
+const currentYear = currentDate.getFullYear();
 
 ReactDOM.render(
   <div>
@@ -14,6 +16,7 @@ ReactDOM.render(
     </h1>
     <p>Random number: {Math.floor(Math.random()*10)}</p>
     <p>Your lucky number is {num}</p>
+    <p>Current year {currentYear}</p>
   </div>,
   document.getElementById("root")
 );
