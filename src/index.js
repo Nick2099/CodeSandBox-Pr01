6c627e0d4278f182a12b6c189d3355f68b589f48
Nikica Dadic <75340386+Nick2099@users.noreply.github.com>
2021-05-18 22:32:13 +0200

    Playing with attributes and styles

--- src/index.js
+++ src/index.js
@@ -6,23 +6,32 @@ const num = 7;
 var currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 
+const customStyle = {
+  color: "indigo",
+  backgroundColor: "silver",
+  fontSize: "2rem",
+  border: "1px solid black"
+};
+
+customStyle.color = "darkgreen";
+
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="false">
       Hello {fName} {lName}
     </h1>
-    <h1>Hello {`${fName} ${lName}`}</h1>
+    <h1 style={{color: "red"}}>Hello {`${fName} ${lName}`}</h1>
     <p>Random number: {Math.floor(Math.random() * 10)}</p>
     <p>Your lucky number is {num}</p>
     <p>Current year {currentYear}</p>
-    <h2>My favorit food</h2>
+    <h2 style={customStyle}>My favorit food</h2>
     <ul>
       <li>Pizza</li>
       <li>Pasta</li>
     </ul>
     <div>
-      <img className="food-img" src="https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg"></img>
-      <img className="food-img" src="https://cdn.gutekueche.de/upload/rezept/1777/spaghetti-carbonara.jpg"></img>
+      <img className="food-img" alt="random photo" src="https://picsum.photos/300/150"></img>
+      <img className="food-img" alt="random photo" src="https://picsum.photos/400/200"></img>
     </div>
   </div>,
   document.getElementById("root")
